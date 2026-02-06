ff36fe1f95cbf8708303350c925784f06e7a37d7
so098 <szerohan@gmail.com>
2026-02-06 23:35:56 +0900

    chore: initialize project configuration

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    ignores: ["build/", "node_modules/"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  }
+);
